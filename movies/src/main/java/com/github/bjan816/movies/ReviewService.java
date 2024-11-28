60364be5adc7a9eaedc0759fb5053d359359866f
bjan816 <bjan816@aucklanduni.ac.nz>
2024-11-28 21:26:43 +0900

    Update ReviewService.java
    Add createReview method

--- movies/src/main/java/com/github/bjan816/movies/ReviewService.java
+++ movies/src/main/java/com/github/bjan816/movies/ReviewService.java
@@ -1,11 +1,28 @@
 package com.github.bjan816.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate; // use the template to form up a new dynamic query and do the job inside the database without using the repository (template is one way of accessing the database, other than using the repository)
+
     public Review createReview(String reviewBody, String imdbId) {
-        Review review = new Review();
+        Review review = reviewRepository.insert(new Review(reviewBody)); // insert() returns the data that has been pushed inside the database
+
+        mongoTemplate.update(Movie.class) // Associate the review with one of the movies. Update the reviewIds array of the movie in the collection
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review)) // Update definition makes the change inside the database
+                .first();
+
         return review;
     }
 }
