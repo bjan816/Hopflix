49188e611c28b0a346fb59cf6d596843f8191fc4
bjan816 <bjan816@aucklanduni.ac.nz>
2024-11-28 17:10:19 +0900

    Add getSingleMovie API

--- movies/src/main/java/com/github/bjan816/movies/MovieController.java
+++ movies/src/main/java/com/github/bjan816/movies/MovieController.java
@@ -1,13 +1,16 @@
 package com.github.bjan816.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,5 +22,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK); // OK means 200
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) { // @PathVariable lets the framework know that whatever we are getting through the path variable {id}, covert it to an ObjectId called 'id'
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
     }
 }
--- movies/src/main/java/com/github/bjan816/movies/MovieService.java
+++ movies/src/main/java/com/github/bjan816/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.github.bjan816.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService { // this class includes the database access methods
@@ -13,5 +15,10 @@ public class MovieService { // this class includes the database access methods
 
     public List<Movie> allMovies() {
         return movieRepository.findAll(); // return a list of the data type (Movie)
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        // The findById method may not find a movie if the id passed doesn't exist. To handle such cases, let Java know it may return null by using Optional.
+        return movieRepository.findById(id);
     }
 }
