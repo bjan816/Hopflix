bfff428b1d37db53b2dfbff4d99a3e5e665305ae
bjan816 <bjan816@aucklanduni.ac.nz>
2024-11-30 00:43:34 +0900

    Update Trailer.js

--- MovieClient/movie-gold-v1/src/components/trailer/Trailer.js
+++ MovieClient/movie-gold-v1/src/components/trailer/Trailer.js
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
-import './Trailer.css'
+import './Trailer.css';
 
 import React from 'react'
 
 const Trailer = () => {
 
     let params = useParams();
-    const key = params.ytTrailerId;
+    let key = params.ytTrailerId;
 
     return (
         <div className="react-player-container">
