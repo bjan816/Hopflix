570839f46527188aefe9cf12135c7ea67f3eda63
bjan816 <bjan816@aucklanduni.ac.nz>
2024-11-30 00:57:56 +0900

    Update Hero.js

--- MovieClient/movie-gold-v1/src/components/hero/Hero.js
+++ MovieClient/movie-gold-v1/src/components/hero/Hero.js
@@ -3,6 +3,7 @@ import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Hero = ({movies}) => {
   return (
@@ -11,7 +12,7 @@ const Hero = ({movies}) => {
             {
                 movies?.map((movie) => { // The optional chaining operator (?.) checks if movies is not null or undefined before calling .map() on it
                     return(
-                        <Paper>
+                        <Paper key={movie.imdbId}>
                             <div className='movie-card-container'>
                                 <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
@@ -22,11 +23,13 @@ const Hero = ({movies}) => {
                                             <h4>{movie.title}</h4>
                                         </div>
                                         <div className='movie-buttons-container'>
+                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>                                            
                                             <div className='play-button-icon-container'>
                                                 <FontAwesomeIcon className='play-button-icon'
                                                     icon={faCirclePlay}
                                                 />
                                             </div>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
