986a6c28ac9e43288f31d1c9e838bad317b59f31
bjan816 <bjan816@aucklanduni.ac.nz>
2024-12-01 02:20:11 +0900

    Update style

--- MovieClient/movie-gold-v1/src/components/header/Header.js
+++ MovieClient/movie-gold-v1/src/components/header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
-            <Navbar.Brand href="/" style={{"color": 'gold'}}>
+            <Navbar.Brand href="/" style={{"color": '#ff7f50'}}>
                 🐇 Hopflix
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
