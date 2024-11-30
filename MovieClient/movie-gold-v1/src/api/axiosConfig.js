1ccc6b10e92568c25375d2a29b62b712cc31d26c
bjan816 <bjan816@aucklanduni.ac.nz>
2024-12-01 00:34:40 +0900

    Update baseURL for deployment

--- MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8080", // Replace ngrok URL with localhost
+    baseURL: "https://hopflix.onrender.com", // Replace ngrok URL with localhost
     timeout: 20000, // Optional: set a timeout for requests
     headers: { "Content-Type": "application/json" } // Define the content type for requests
 });
